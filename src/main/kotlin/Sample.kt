4fc58202d049dd9729654d317ab06481d022420e
Lukas Kusik <lukas.kusik@gmail.com>
2022-08-06 20:29:02 +0200

    Provide initial sample build.gradle

---
+++ src/main/kotlin/Sample.kt
@@ -0,0 +1,45 @@
+object Sample {
+    const val GROOVY = """plugins {
+    id 'com.android.application'
+}
+
+android {
+    namespace 'com.example.myapp'
+    compileSdkVersion 28
+    defaultConfig {
+        applicationId 'com.example.myapp'
+        minSdkVersion 15
+        targetSdkVersion 28
+        versionCode 1
+        versionName "1.0"
+    }
+
+    buildTypes {
+        release {
+            minifyEnabled true
+            proguardFiles getDefaultProguardFile('proguard-android.txt'), 'proguard-rules.pro'
+        }
+    }
+
+    flavorDimensions "tier"
+    productFlavors {
+        free {
+            dimension "tier"
+            applicationId 'com.example.myapp.free'
+        }
+
+        paid {
+            dimension "tier"
+            applicationId 'com.example.myapp.paid'
+        }
+    }
+}
+
+dependencies {
+    implementation project(":lib")
+    implementation 'com.android.support:appcompat-v7:28.0.0'
+    implementation fileTree(dir: 'libs', include: ['*.jar'])
+}
+
+    """
+}
--- src/main/kotlin/client.kt
+++ src/main/kotlin/client.kt
@@ -9,6 +9,10 @@ fun main() {
         val groovyInput = document.getElementById("groovyInput") as HTMLTextAreaElement
         val kotlinOutput = document.getElementById("kotlinOutput") as HTMLTextAreaElement
 
+        // Initial sample
+        groovyInput.textContent = Sample.GROOVY
+        kotlinOutput.textContent = GradleKotlinConverter.convert(Sample.GROOVY)
+
         val convert: (Event) -> dynamic = {
             kotlinOutput.textContent = GradleKotlinConverter.convert(groovyInput.value)
             Unit.asDynamic()
@@ -17,8 +21,27 @@ fun main() {
         groovyInput.onkeyup = convert
         groovyInput.onchange = convert
 
+        // Delete on initial click
+        var clicked = false
+        groovyInput.onclick = {
+            if (!clicked) {
+                groovyInput.textContent = ""
+                kotlinOutput.textContent = ""
+                clicked = true
+            }
+        }
+
 
         Unit.asDynamic()
     }
 
+}
+
+fun sendConversionEvent() {
+    js(
+        """
+        logEvent(analytics, 'conversion', {
+        });
+    """
+    )
 }
